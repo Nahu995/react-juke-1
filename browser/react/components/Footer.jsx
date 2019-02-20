9e65d23ae0378c7d8f90fb76fdba8e5b3d9fb950
Nahuel <nahu995@gmail.com>
2019-02-20 14:23:05 -0300

    second commit

---
+++ browser/react/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default () => (
+  <footer>
+    <div className="pull-left">
+      <button className="btn btn-default">
+        <span className="glyphicon glyphicon-step-backward"></span>
+      </button>
+      <button className="btn btn-default">
+        <span className="glyphicon glyphicon-play"></span>
+      </button>
+      <button className="btn btn-default">
+        <span className="glyphicon glyphicon-step-forward"></span>
+      </button>
+    </div>
+    <div className="bar">
+      <div className="progress">
+        <div className="progress-bar"></div>
+      </div>
+    </div>
+</footer>
+)
--- browser/react/index.js
+++ browser/react/index.js
@@ -1,1 +1,5 @@
-console.log('Hello React');
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Main from './containers/Main'
+
+ReactDOM.render(<Main />, document.getElementById('app'));
